e54cdd689bb62cca20a8207389b787e2774602dd
abidplabon <mohammedplabon@gmail.com>
2021-06-19 14:03:50 -0700

    End of Section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity } from '../models/activity';
+import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
 
 const sleep=(delay:number)=>{
@@ -11,6 +12,12 @@ const sleep=(delay:number)=>{
 }
 
 axios.defaults.baseURL='http://localhost:5000/api';
+
+axios.interceptors.request.use(config=>{
+    const token=store.commonStore.token;
+    if(token) config.headers.Authorization=`Bearer ${token}`
+    return config;
+})
 
 const responseBody=<T>(response:AxiosResponse<T>)=>response.data;
 
@@ -65,7 +72,13 @@ const Activities={
     update:(activity:Activity)=>requests.put<Activity>(`/activities/${activity.id}`,activity),
     delete:(id:string)=>axios.delete<void>(`/activities/${id}`)
 }
+const Account ={
+    current:()=>requests.get<User>('/account'),
+    login:(user:UserFormValues)=>requests.post<User>('/account/login',user),
+    register:(user:UserFormValues)=>requests.post<User>('/account/register',user)
+}
 const agent ={
-    Activities
+    Activities,
+    Account
 }
 export default agent;
