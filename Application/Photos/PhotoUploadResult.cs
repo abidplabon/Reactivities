1cb041b551f098fd18355fb954604496be5c8935
abidplabon <mohammedplabon@gmail.com>
2021-06-24 23:15:33 -0700

    End of Section 16

---
+++ Application/Photos/PhotoUploadResult.cs
@@ -0,0 +1,10 @@
+using Application.Interfaces;
+
+namespace Application.Photos
+{
+    public class PhotoUploadResult
+    {
+        public string PublicId { get; set; }
+        public string Url { get; set; }
+    }
+}
